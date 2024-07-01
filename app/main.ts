ec8631939a7191e68beb2a02488a37a66b964153
Franklin Antony <franklinanton.007@gmail.com>
2024-07-01 21:21:47 +0100

    pass 2n stage

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,11 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+  console.log("data received");
   connection.on("data", (data: Buffer) => {
-	connection.write(`+PONG\r\n`);
+    console.log(data.toString());
+	  connection.write(`+PONG\r\n`);
+    connection.end();
   });
 });
 
