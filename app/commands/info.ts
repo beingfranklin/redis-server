90efbaae5109b648abaa0edc9d03d54aa4190ed6
Franklin Antony <franklinanton.007@gmail.com>
2024-07-13 17:40:29 +0100

    add info command and replica of

--- app/commands/info.ts
+++ app/commands/info.ts
@@ -1,5 +1,10 @@
 import type { Socket } from "node:net";
-import { bulkString, endOfString } from "../helpers/common";
+import {
+	bulkString,
+	endOfString,
+	MASTER_REPL_OFFSET,
+	MASTER_REPLID,
+} from "../helpers/common";
 
 export const handleInfo = (
 	connection: Socket,
@@ -9,7 +14,7 @@ export const handleInfo = (
 	const section = dataString[4]?.toLowerCase();
 
 	if (section === "replication") {
-		const infoResponse = `role:${role}`;
+		const infoResponse = `role:${role}\nmaster_replid:${MASTER_REPLID}\nmaster_repl_offset:${MASTER_REPL_OFFSET}`;
 		connection.write(bulkString(infoResponse));
 	} else {
 		connection.write(`$-1${endOfString}`);
--- app/helpers/common.ts
+++ app/helpers/common.ts
@@ -3,3 +3,5 @@ export const endOfString = "\r\n";
 export const nullString = `$-1${endOfString}`;
 export const bulkString = (response: string) =>
 	`$${response.length}\r\n${response}${endOfString}`;
+export const MASTER_REPLID = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+export const MASTER_REPL_OFFSET = 0;
